ec6413546dfbd3080de9562a53489b0f87b2d346
Qyce Broussard <qbroussard18@gmail.com>
2026-06-24 19:15:55 -0500

    Completed schema and seeded the database

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,32 @@
--- TODO
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS orders_products;
+
+CREATE TABLE users(
+    id serial PRIMARY KEY,
+    username text UNIQUE NOT NULL,
+    password text NOT NULL
+);
+
+CREATE TABLE orders(
+    id serial PRIMARY KEY,
+    date date NOT NULL,
+    note text NOT NULL,
+    user_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE products(
+    id serial PRIMARY KEY,
+    title text NOT NULL,
+    description text NOT NULL,
+    price decimal NOT NULL
+);
+
+
+CREATE TABLE orders_products(
+    order_id integer NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
+    product_id integer NOT NULL REFERENCES products(id) ON DELETE CASCADE,
+    quantity integer NOT NULL,
+    UNIQUE (order_id, product_id)
+);
--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,25 @@
 import db from "#db/client";
 
+import { createUser } from "#db/queries/users";
+import { createOrder } from "#db/queries/orders";
+import { createProduct } from "#db/queries/products";
+import { createOrderProduct } from "#db/queries/orders_products";
+
 await db.connect();
 await seed();
 await db.end();
 console.log("🌱 Database seeded.");
 
 async function seed() {
-  // TODO
+  for (let i = 1; i <= 10; i++) {
+    await createProduct("Product " + i, "Description " + i, i * 5);
+  }
+
+  for (let i = 1; i <= 2; i++) {
+    const user = await createUser("Account " + i, "password123");
+    await createOrder("2026-6-18", "Note " + i, user.id);
+    for (let j = 1; j <= 5; j++) {
+      await createOrderProduct(i, j, j * 2);
+    }
+  }
 }
